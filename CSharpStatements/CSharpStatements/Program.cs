c11f2159b1c2d9fd24025a7bb2fa15c67c0716e2
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-12-15 18:58:45 +0200

    Test if a random number is even or odd

--- CSharpStatements/CSharpStatements/Program.cs
+++ CSharpStatements/CSharpStatements/Program.cs
@@ -6,7 +6,17 @@ namespace CSharpStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random randomizer = new Random();
+            int i = randomizer.Next(0, 100);
+
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is Even");
+            }
+            else
+            {
+                Console.WriteLine($"{i} is Odd");
+            }
         }
     }
 }
